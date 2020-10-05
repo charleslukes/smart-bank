3c872c9cfb19644889abcd474b76b5d81e7d6fd6
charleslukes <charleslukes28@gmail.com>
2020-10-05 10:47:09 +0100

    add navigation on app

--- App.js
+++ App.js
@@ -6,13 +6,24 @@
  * @flow strict-local
  */
 
+import 'react-native-gesture-handler';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import SignIn from './lib/signin';
+import Home from './lib/welcome';
+
+const Stack = createStackNavigator();
 
 const App: () => React$Node = () => {
   return (
     <>
-      <SignIn />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="SignIn" component={SignIn} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 };
